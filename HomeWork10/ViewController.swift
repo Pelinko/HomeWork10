005ba9e1450e359bd53dd30414cf3509f62e9a6a
Dmitrii Pelinko <pelinko2015@yandex.ru>
2022-02-04 16:00:47 +0700

    Deleting comments

--- HomeWork10/ViewController.swift
+++ HomeWork10/ViewController.swift
@@ -1,9 +1,4 @@
-//
-//  ViewController.swift
-//  HomeWork10
-//
-//  Created by Дмитрий  Пелинко on 04.02.2022.
-//
+
 
 import UIKit
 
@@ -11,7 +6,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
